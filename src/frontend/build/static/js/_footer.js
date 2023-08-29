8caff0da873260105fed4431136756df10d7d7c4
Chyna D <cdavis2757@gmail.com>
2023-08-29 01:50:44 -0700

    Added html footer module

--- src/frontend/build/static/js/_footer.js
+++ src/frontend/build/static/js/_footer.js
@@ -1,36 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const footerContainer = document.getElementById('footer-container');
-
-    // Create footer elements
-    const footer = document.createElement('footer');
-    footer.classList.add('site-footer');
-
-    const container = document.createElement('div');
-    container.classList.add('container');
-    
-    const footerContent = document.createElement('div');
-    footerContent.classList.add('footer-content');
-
-    const logo = document.createElement('div');
-    logo.classList.add('footer-logo');
-
-    const footerLinks = document.createElement('div');
-    footerLinks.classList.add('footer-links');
-
-    const footerAddress = document.createElement('div');
-    footerAddress.classList.add('footer-address');
-
-    const footerBottom = document.createElement('div');
-    footerBottom.classList.add('footer-bottom');
-
-    // Append the content to the footer
-    footer.appendChild(container);
-    footer.appendChild(footerContent);
-    footer.appendChild(logo);
-    footer.appendChild(footerLinks);
-    footer.appendChild(footerAddress);
-    footer.appendChild(footerBottom);
-
-    // Insert the footer into the footer-container element
-    footerContainer.appendChild(footer);
-});
+// INJECT SITE FOOTER TO EVERY PAGE
+const footer = document.querySelector(".page-footer");
+fetch("./pages/_footer.html")
+    .then(response => response.text())
+    .then(data => {
+        footer.innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Error fetching footer content:", error);
+    });
